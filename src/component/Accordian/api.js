3a916757399b499862e834cdbd841f1e290db35c
bhupendragtm <bhupendragtm777@gmail.com>
2022-12-19 19:01:50 +0000

    Items Were Shown

--- src/component/Accordian/api.js
+++ src/component/Accordian/api.js
@@ -2,21 +2,21 @@ import  { useState, useEffect } from "react";
 import axios from "axios";
 
 const Posts = () =>{
-  const [items, setItems] = useState(['bhoj']);
+  let itemss = [];
+  const [items, setItems] = useState([]);
   const getPosts = () => {
     axios
     .get("https://localhost/wordpress5/wp-json/wp/v2/posts")
     .then((response) => {
       // console.log('Response Is Here'+JSON.stringify(response));
       response.data.forEach((i) => {
-        let c = i.title.rendered;
         console.log( 'This is I'+i);
         console.log( 'Links are Here'+i._links.about);
         console.log( i.title.rendered);
-        
+        itemss.push(i.title.rendered);
         console.log("Items are Here"+items);
         // return(i.title.rendered);
-        setItems([...items, c]);
+        setItems([itemss]);
       });
     })
     .catch((error) => {
